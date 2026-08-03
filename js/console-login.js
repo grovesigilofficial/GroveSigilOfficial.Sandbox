45681b4495acac7e7be9243e153a30a26d136923
GroveSigilOfficial <grovesigilofficial@gmail.com>
2026-08-03 15:18:31 -0700

    Refactor login logic to use groveClient

--- js/console-login.js
+++ js/console-login.js
@@ -1,9 +1,14 @@
+console.log("console-login.js loaded");
+
+
 const form = document.querySelector("#loginForm");
 
 
 form.addEventListener("submit", async function (event) {
 
+
     event.preventDefault();
+
 
 
     const email = document.querySelector("#email").value.trim();
@@ -11,9 +16,16 @@ form.addEventListener("submit", async function (event) {
     const password = document.querySelector("#password").value;
 
 
+
     try {
 
-        const { data, error } = await window.grove.auth.signInWithPassword({
+
+        console.log("Grove Client:", window.groveClient);
+
+
+
+        const { data, error } =
+        await window.groveClient.auth.signInWithPassword({
 
             email,
             password
@@ -21,41 +33,53 @@ form.addEventListener("submit", async function (event) {
         });
 
 
-        if (error) {
+
+        if(error){
 
             alert(error.message);
+
             return;
 
         }
+
 
 
         const user = data.user;
 
 
-        if (!user.email_confirmed_at) {
+
+        if(!user.email_confirmed_at){
 
             alert("Please confirm your email before logging in.");
 
-            await window.grove.auth.signOut();
+
+            await window.groveClient.auth.signOut();
+
 
             return;
 
         }
 
 
+
         alert("Login successful.");
 
+
         window.location.href = "./console.html";
+
 
 
     }
 
 
-    catch (err) {
+    catch(err){
 
-        console.error(err);
+
+        console.error("Console login error:", err);
+
 
         alert(err.message);
+
 
     }
 
