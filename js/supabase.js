617564c667e36575aa715bb334a0e35ff9162ab5
GroveSigilOfficial <grovesigilofficial@gmail.com>
2026-08-03 13:38:47 -0700

    Fix client creation for Supabase

--- js/supabase.js
+++ js/supabase.js
@@ -2,7 +2,11 @@ const SUPABASE_URL = "https://izhdxcfinxzlziynbuvx.supabase.co";
 
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6aGR4Y2Zpbnh6bHppeW5idXZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU0NDcyODIsImV4cCI6MjEwMTAyMzI4Mn0.t-34zI46FOK0KAKlVx4OzyBVnYgjM-0sb6tb1OIGqGo";
 
-window.grove = window.supabase.createClient(
+
+const supabase = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
+
+
+console.log("Supabase client loaded:", supabase);
