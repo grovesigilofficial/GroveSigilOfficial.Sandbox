79cf9d58336f38c5b96234ff751354f418e91f82
GroveSigilOfficial <grovesigilofficial@gmail.com>
2026-08-03 13:47:45 -0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,4 @@
-console.log("supabase.js loading");
+console.log("supabase.js loaded");
 
 
 const SUPABASE_URL = "https://izhdxcfinxzlziynbuvx.supabase.co";
@@ -6,15 +6,10 @@ const SUPABASE_URL = "https://izhdxcfinxzlziynbuvx.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6aGR4Y2Zpbnh6bHppeW5idXZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU0NDcyODIsImV4cCI6MjEwMTAyMzI4Mn0.t-34zI46FOK0KAKlVx4OzyBVnYgjM-0sb6tb1OIGqGo";
 
 
-const client = window.supabase.createClient(
+window.groveClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
 
 
-window.groveClient = client;
-
-
-console.log("CLIENT:", client);
-
-console.log("AUTH:", client.auth);
+console.log("Grove client created:", window.groveClient);
