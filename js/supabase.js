804aba06117fccfb60abb7f7a87770d59ed6e66c
GroveSigilOfficial <grovesigilofficial@gmail.com>
2026-08-09 08:47:22 -0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,15 +1,20 @@
+```javascript
 console.log("supabase.js loaded");
 
+const SUPABASE_URL =
+"https://izhdxcfinxzlziynbuvx.supabase.co";
 
-const SUPABASE_URL = "https://izhdxcfinxzlziynbuvx.supabase.co";
+const SUPABASE_ANON_KEY =
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6aGR4Y2Zpbnh6bHppeW5idXZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU0NDcyODIsImV4cCI6MjEwMTAyMzI4Mn0.t-34zI46FOK0KAKlVx4OzyBVnYgjM-0sb6tb1OIGqGo";
 
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6aGR4Y2Zpbnh6bHppeW5idXZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU0NDcyODIsImV4cCI6MjEwMTAyMzI4Mn0.t-34zI46FOK0KAKlVx4OzyBVnYgjM-0sb6tb1OIGqGo";
-
-
-window.groveClient = window.supabase.createClient(
+window.groveClient =
+window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
 
-
-console.log("Grove client created:", window.groveClient);
+console.log(
+    "Grove client created:",
+    window.groveClient
+);
+```
